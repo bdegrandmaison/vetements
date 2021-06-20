f0cd46298839ede4cf754432bf718a4db4e0d715
B. de Grandmaison <bdegrandmaison@hotmail.fr>
2021-06-20 21:07:14 +0200

    Backend URL saved

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-export const endpoint = `https://vetementsbackend.herokuapp.com:30376/api/graphql`;
-export const prodEndpoint = `https://vetementsbackend.herokuapp.com:30376/api/graphql`;
+export const endpoint = `https://vetementsbackend.herokuapp.com/api/graphql`;
+export const prodEndpoint = `https://vetementsbackend.herokuapp.com/api/graphql`;
 export const perPage = 2;
