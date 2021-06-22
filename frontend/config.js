f2d20d16b68dba8345ab749297b2abee44747810
B. de Grandmaison <bdegrandmaison@hotmail.fr>
2021-06-22 13:51:49 +0200

    Changed config

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `http://localhost:3000/api/graphql`;
+export const endpoint = `http://127.0.0.1/calls`;
+export const prodEndpoint = `http://127.0.0.1/calls`;
 export const perPage = 4;
